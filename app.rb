dd6d3f7213cedf20b2129c73f44b52c7aa747227
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-06-29 10:23:23 +0300

    modified app.rb

---
+++ app.rb
@@ -0,0 +1,16 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/coin_combination')
+also_reload('lib/**/*.rb')
+
+get('/')do
+  erb(:index)
+end
+
+get('/change')do
+  @change= params.fetch("cents").to_i.coin_combination()
+  erb(:change)
+end
+
+
+
